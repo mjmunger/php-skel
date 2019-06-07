ae4187dff732c5e77256a45db58879fe628b12c4
michael <mj@hph.io>
2019-06-07 13:06:17 -0400

    protected my original development directory.

--- buildskel.sh
+++ buildskel.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
-mkdir bootstrap build config src tests tools uploads
-cd tests
-mkdir integration unit
-cd ..
-touch .htaccess CHANGELOG.md README.md 
+function build_skel() {
+    mkdir bootstrap build config src tests tools uploads
+    cd tests
+    mkdir integration unit
+    cd ..
+    touch .htaccess CHANGELOG.md README.md
+}
 
-PACKAGES=( league/route league/container zendframework/zend-diactoros zendframework/zend-httphandlerrunner monolog/monolog guzzlehttp/guzzle hphio/api-cli hphio/util filp/whoops ext-pdo hphio/farret phpmailer/phpmailer )
+function run_composer() {
 
-for PACKAGE in ${PACKAGES[@]}
-do
-  composer require ${PACKAGE}
-done
+    PACKAGES=( league/route league/container zendframework/zend-diactoros zendframework/zend-httphandlerrunner monolog/monolog guzzlehttp/guzzle hphio/api-cli hphio/util filp/whoops ext-pdo hphio/farret phpmailer/phpmailer )
 
-DEVPACKAGES=( phpunit/phpunit )
+    for PACKAGE in ${PACKAGES[@]}
+    do
+      composer require ${PACKAGE}
+    done
 
-for PACKAGE in ${DEVPACKAGES[@]}
-do
-  composer require --dev ${PACKAGE}
-done
+    DEVPACKAGES=( phpunit/phpunit )
 
-ln -s vendor/phpunit/phpunit/phpunit .
-chmod ./phpunit
+    for PACKAGE in ${DEVPACKAGES[@]}
+    do
+      composer require --dev ${PACKAGE}
+    done
+}
+
+function setup_phpunit () {
+    ln -s vendor/phpunit/phpunit/phpunit .
+    chmod ./phpunit
+}
+
+function checkout_repo() {
+#    rm -vfr .git
+#    git clone $1
+    echo "checkout"
+}
+
+function check_original_dev_location() {
+    if [ "/home/michael/bash/php-skel" = "$(pwd)" ]; then
+        echo "You cannot run this in the original development directory. Clone somewhere else."
+        exit 1
+    fi
+}
+
+check_original_dev_location
+
+if [ $# -ne 1 ]; then
+    echo "Usage: buildskel.sh [repo url]"
+    exit 1
+fi
+
+build_skel
+run_composer
+setup_phpunit
+checkout_repo $1
+
+echo "Completed."
